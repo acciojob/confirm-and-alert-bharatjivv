921094e3c2b240ff50ceaf401ec4a43ddf1fa385
Bharat <hibharatj@gmail.com>
2026-01-16 16:35:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Ask the user for confirmation
+let userResponse = confirm("Do you want to proceed?");
+
+// Check the response
+if (userResponse) {
+    alert("You clicked OK. Proceeding...");
+} else {
+    alert("You clicked Cancel. Exiting...");
+}
